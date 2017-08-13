e141b224156e13edf923bfcff7c85b060c7a286b
Artem Konstantinov <don666vito@yandex.ua>
2017-08-13 17:31:16 +0300

    #More changes

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+$('#progressbar1').LineProgressbar({
+    percentage: 70,
+    fillBackgroundColor: '#1abc9c',
+    height: '7px',
+    radius: '15px'
+});
+
+$('#progressbar2').LineProgressbar({
+    percentage: 60,
+    fillBackgroundColor: '#9b59b6',
+    height: '7px',
+    radius: '15px'
+});
+
+$('#progressbar3').LineProgressbar({
+    percentage: 65,
+    fillBackgroundColor: '#f1c40f',
+    height: '7px',
+    radius: '15px'
+});
